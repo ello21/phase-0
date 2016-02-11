41dd1a5e0b7b1aab503db4c0e7467eb173e996f8
ello21 <ello21@users.noreply.github.com>
2016-02-11 18:09:50 -0500

    Initial commit

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,24 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+# def good_guess?(int)
+# 	if int==42
+# 		true
+# 	else false
+# 	end
+# end
+
+def good_guess?(int)
+	unless int==42
+		false
+	else true
+	end
+end
+
+# puts good_guess?(42)
+# puts good_guess?(0)
+# puts good_guess?(-1)
